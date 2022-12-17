8b5a7d3c3281b45713afcb66db6bc7b5e18d2b35
SiddhiKanakiya <siddhikanakiya@gmail.com>
2022-12-17 20:57:02 +0530

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let warningOne = document.querySelector("#wOne");
 let warningTwo = document.querySelector("#wTwo");
+let warningThree = document.querySelector("#wThree");
+
 let submitButton = document.getElementById("submitBtn");
 let email = document.getElementById("emailaddress");
 let password = document.getElementById("password");
@@ -17,6 +19,9 @@ function emailcheck(){
     }
     else{
         warningOne.setAttribute("style","color:red");
+        warningThree.setAttribute("style","color:white");
+        document.querySelector(".Password").style.top= '107px';
+        document.querySelector("#password").style.top= '130px';
         eCheck= false;
     }
 
@@ -37,6 +42,9 @@ function passwordcheck(){
     }
     else{
         warningTwo.setAttribute("style","color:red");
+        warningThree.setAttribute("style","color:white");
+        document.querySelector(".Password").style.top= '107px';
+        document.querySelector("#password").style.top= '130px';
         pCheck = false;
     }
 
@@ -67,7 +75,6 @@ submitButton.onclick = confirmation
 function frame2(){
     warningOne.setAttribute("style","color:white");
     warningTwo.setAttribute("style","color:white");
-    let warningThree = document.querySelector("#wThree");
     warningThree.setAttribute("style","color:green");
 
 
